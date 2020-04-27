470e165e827c3b7f74d3a372cf5a272471853099
Guillermo Hernandez <scienceguillermo@gmail.com>
2020-04-27 16:50:37 -0400

    borrador con imagen animada

--- app/src/main/java/com/example/borrador/MainActivity.java
+++ app/src/main/java/com/example/borrador/MainActivity.java
@@ -5,22 +5,37 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+
+import com.felipecsl.gifimageview.library.GifImageView;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.io.IOUtil;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    private GifImageView gifImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //llamado de la funcion de limpieza al abrir el app Borrador
+        gifImageView =findViewById(R.id.gifImageView);
+        try{
+            InputStream inputStream = getAssets().open("borrado.gif");
+            byte[] bytes = IOUtil.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+        catch (IOException ex)
+        {
+        }
         clearPreferences();
     }
 
@@ -48,6 +63,17 @@ public class MainActivity extends AppCompatActivity {
                     DataOutputStream dos = new DataOutputStream(p.getOutputStream());
                     //instruccion de borrado mediante nombre de paquete, se puede enviar n cantidad de paquetes
                     dos.writeBytes("pm clear com.netflix.mediaclient\n");
+                    dos.writeBytes("pm clear com.hbo.android.app\n");
+                    dos.writeBytes("pm clear com.amazon.avod.thirdpartyclient\n");
+                    dos.writeBytes("pm clear tv.wuaki\n");
+                    dos.writeBytes("pm clear com.dazn\n");
+                    dos.writeBytes("pm clear es.plus.yomvi\n");
+                    dos.writeBytes("pm clear ptv.bein.mena\n");
+                    dos.writeBytes("pm clear com.spotify.music\n");
+                    dos.writeBytes("pm clear com.amazon.mp3\n");
+                    dos.writeBytes("pm clear com.soundcloud.android\n");
+                    dos.writeBytes("pm clear deezer.android.app\n");
+                    dos.writeBytes("pm clear com.aspiro.tidal\n");
                     dos.writeBytes("exit\n");
                     dos.flush();
                     dos.close();
@@ -57,6 +83,12 @@ public class MainActivity extends AppCompatActivity {
                     //cerrar la aplicacion luego de ejecutar el proceso de borrado
                     int q = android.os.Process.myPid();
                     android.os.Process.killProcess(q);*/
+
+            new Handler().postDelayed(new Runnable(){
+                public void run(){
+                Destroy();
+                }
+            }, 3000);
                 } catch (IOException e) {
                     /*//ir atras para minimizar la aplicacion
                     moveTaskToBack(true);
@@ -64,6 +96,12 @@ public class MainActivity extends AppCompatActivity {
                     int q = android.os.Process.myPid();
                     android.os.Process.killProcess(q);*/
                     e.printStackTrace();
+
+            new Handler().postDelayed(new Runnable(){
+                public void run(){
+                    Destroy();
+                }
+            }, 3000);
 
                 } catch (InterruptedException e) {
                     /*//ir atras para minimizar la aplicacion
@@ -73,9 +111,22 @@ public class MainActivity extends AppCompatActivity {
                     android.os.Process.killProcess(q);*/
                     e.printStackTrace();
 
+            new Handler().postDelayed(new Runnable(){
+                public void run(){
+                    Destroy();
+                }
+            }, 3000);
+
                 }
             }
 
 
+    protected void Destroy() {
+        //ir atras para minimizar la aplicacion
+        moveTaskToBack(true);
+        //cerrar la aplicacion luego de ejecutar el proceso de borrado
+        int q = android.os.Process.myPid();
+        android.os.Process.killProcess(q);
+    }
     }
 
